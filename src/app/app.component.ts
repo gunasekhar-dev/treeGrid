6030b2e46bf4ac5079afe06dd089861ce9e56da9
gunasekhar-dev <80669471+gunasekhar-dev@users.noreply.github.com>
2023-04-17 16:14:22 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { sampleData } from './datasource';
+import {
+  PageService,
+  PageSettingsModel,
+} from '@syncfusion/ej2-angular-treegrid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'treeGrid';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.data = sampleData;
+    this.pageSettings = { pageSize: 6 };
+  }
+  public data!: Object[];
+
+  pageSettings!: PageSettingsModel;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeGridModule, PageService } from '@syncfusion/ej2-angular-treegrid';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TreeGridModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
